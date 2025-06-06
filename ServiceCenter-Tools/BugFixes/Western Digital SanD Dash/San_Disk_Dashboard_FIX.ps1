7ba7daa0537492f7d14e6a16b7c93027ee971ee5
Seth Burns <Zeth987@gmail.com>
2025-06-05 18:25:20 -0600

    Reorganization and updates

---
+++ ServiceCenter-Tools/BugFixes/Western Digital SanD Dash/San_Disk_Dashboard_FIX.ps1
@@ -0,0 +1,42 @@
+Powershell
+
+#region Script Info
+$Script_Name = "San_Disk_Dashboard_FIX.ps1"
+$Description = "Script will run san disk dashboard fix. Update Firmware for drive once it has loaded."
+$Author = "Seth Burns - System Administrator II - Service Center"
+$last_tested = "05-27-25"
+$version = "1.0.0"
+$live = "WIP"
+$bmgr = "WIP"
+#endregion
+
+#region Text Colors 
+function Red     { process { Write-Host $_ -ForegroundColor Red }}
+function Green   { process { Write-Host $_ -ForegroundColor Green }}
+function Yellow  { process { Write-Host $_ -ForegroundColor Yellow }}
+function Blue    { process { Write-Host $_ -ForegroundColor Blue }}
+function Cyan    { process { Write-Host $_ -ForegroundColor Cyan }}
+function Magenta { process { Write-Host $_ -ForegroundColor Magenta }}
+function White   { process { Write-Host $_ -ForegroundColor White }}
+function Gray    { process { Write-Host $_ -ForegroundColor Gray }}
+#endregion
+
+#region Main Descriptor
+## START Main Descriptor
+Write-Output "--------------------" | Yellow
+Write-Output "$Author" | Yellow
+Write-Output "$Script_Name" | Yellow
+Write-Output "$version , $last_tested" | Yellow
+Write-Output "$live , $bmgr" | Yellow
+Write-Output "$Description" | Yellow
+Write-Output "--------------------" | Yellow
+## END Main Descriptor
+#endregion
+
+
+
+cmd.exe
+SET QTWEBENGINE_DISABLE_SANDBOX=1
+cd "C:\Program Files (x86)\SanDisk\Dashboard"
+Dashboard.exe
+
