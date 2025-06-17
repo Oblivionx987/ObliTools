bcbe3dd28c00cd868a1fc24851cc97f399398b04
Seth Burns <Zeth987@gmail.com>
2025-06-17 15:48:45 -0600

    foldermaps update

--- ServiceCenter-Tools/Group Policy/FolderMapping/Foldermaps2.ps1
+++ ServiceCenter-Tools/Group Policy/FolderMapping/Foldermaps2.ps1
@@ -26,3 +26,40 @@ foreach ($drive in $mappedDrives) {
 
 # Output the list of mapped drives
 $mappedDrivesInfo | Format-Table -AutoSize
+
+# Get all physical drives (local and network) using Get-PSDrive
+$allDrives = Get-PSDrive | Where-Object { $_.Provider.Name -eq 'FileSystem' }
+$physicalDrivesInfo = $allDrives | ForEach-Object {
+    [PSCustomObject]@{
+        DriveLetter = $_.Name
+        Root        = $_.Root
+        Provider    = $_.Provider.Name
+        Description = $_.Description
+    }
+}
+
+# Output the list of physical drives
+Write-Host "Physical Drives:" -ForegroundColor Cyan
+$physicalDrivesInfo | Format-Table -AutoSize
+
+# Compare mapped drives and physical drives for conflicts
+$conflicts = @()
+foreach ($mapped in $mappedDrivesInfo) {
+    foreach ($physical in $physicalDrivesInfo) {
+        if ($mapped.DriveLetter -eq $physical.DriveLetter) {
+            $conflicts += [PSCustomObject]@{
+                DriveLetter = $mapped.DriveLetter
+                MappedPath  = $mapped.RemotePath
+                PhysicalRoot = $physical.Root
+                Issue = "Drive letter conflict (Mapped and Physical)"
+            }
+        }
+    }
+}
+
+if ($conflicts.Count -gt 0) {
+    Write-Host "Conflicts Detected:" -ForegroundColor Red
+    $conflicts | Format-Table -AutoSize
+} else {
+    Write-Host "No drive letter conflicts detected." -ForegroundColor Green
+}
